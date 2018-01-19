3a0dbba739b9376283046e11c39d48cfbaa82d16
Huy <kute2110@primarynet.co.kr>
2018-01-19 15:11:41 +0700

    Insert database by Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        //$this->call(UsersSeeder::class);
-        //$this->call(SettingSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(SettingSeeder::class);
         $this->call(EvtlistSeeder::class);
     }
 }
